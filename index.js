5f96bdefd7b568397dab83d69b4e4df1867d4160
Iuri Matias <iuri.matias@gmail.com>
2017-01-15 14:23:45 -0500

    update list of matching files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("babel-preset-es2017");
 
 module.exports = function(embark) {
 
-  embark.registerPipeline((embark.pluginConfig.files || ['*.js', '*.jsx']), function(opts) {
+  embark.registerPipeline((embark.pluginConfig.files || ['**/*.js', '**/*.jsx']), function(opts) {
     var targetFile = opts.targetFile;
     var source = opts.source;
     var presets = embark.pluginConfig.presets || ['es2015', 'es2016', 'es2017', 'react'];
