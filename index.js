c5c685275b24ad0852032124b8ba7e8e9faffb1f
Iuri Matias <iuri.matias@gmail.com>
2017-01-15 12:19:53 -0500

    add support for the various es6 presets

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var babel = require("babel-core");
+require("babel-preset-react");
+require("babel-preset-es2015");
+require("babel-preset-es2016");
+require("babel-preset-es2017");
 
 module.exports = function(embark) {
 
@@ -10,7 +14,7 @@ module.exports = function(embark) {
       return source;
     }
 
-    return babel.transform(source, {}).code;
+    return babel.transform(source, {minified: true, presets: ['es2015', 'es2016', 'es2017', 'react']}).code;
   });
 
 };
