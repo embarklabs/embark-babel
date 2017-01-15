02c46cd72a6c39dd38551abd3cf0e7ab90e2d908
Iuri Matias <iuri.matias@gmail.com>
2017-01-15 13:53:35 -0500

    specify file types

--- index.js
+++ index.js
@@ -6,15 +6,11 @@ require("babel-preset-es2017");
 
 module.exports = function(embark) {
 
-  embark.registerPipeline(function(opts) {
+  embark.registerPipeline((embark.pluginConfig.files || ['*.js', '*.jsx']), function(opts) {
     var targetFile = opts.targetFile;
     var source = opts.source;
     var presets = embark.pluginConfig.presets || ['es2015', 'es2016', 'es2017', 'react'];
     var minified = (embark.pluginConfig.minified !== undefined ? embark.pluginConfig.minified : true);
-
-    if (targetFile.split('.').slice(-1)[0] !== 'js' ) {
-      return source;
-    }
 
     return babel.transform(source, {minified: minified, presets: presets}).code;
   });
